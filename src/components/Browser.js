b20fa92511112b2ef03dbbf34189ffd7c9be2af8
avergel <alberto.vergel@gmail.com>
2019-11-25 16:52:40 +0100

    Refactor

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -16,7 +16,6 @@ const Browser = (props) => {
 
   useEffect(() => {
     props.listFiles(prefixPath, props.userToken, props.refreshToken)
-      // props.listFiles(prefixPath, 'eyJraWQiOiJzK0pyYmpEbGtIOFFFaVwvTEpwbEt1RFdEeTYyTTQ2RWFkNVJUWTgxeGdNZz0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI0N2E3OTE2NC04ZWZhLTRjNWUtODdlYS1lNDNkN2MwMTNlZjIiLCJhdWQiOiI2NHFjZGY1ZjVtcmMxbThrcjFvc3RzYzlqNSIsImV2ZW50X2lkIjoiZDZjMWM5M2MtZDQ4My00ZjIwLTgzNzAtZDQxMzE0MjA5MGZiIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE1NzMzMjYxMjMsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5ldS13ZXN0LTEuYW1hem9uYXdzLmNvbVwvZXUtd2VzdC0xX0JpanFwRjdoMCIsImNvZ25pdG86dXNlcm5hbWUiOiI0N2E3OTE2NC04ZWZhLTRjNWUtODdlYS1lNDNkN2MwMTNlZjIiLCJleHAiOjE1NzM1ODM4NjYsImlhdCI6MTU3MzU4MDI2NywiZW1haWwiOiJzdHJhdy50b3JvQGdtYWlsLmNvbSJ9.Nyq2TM5DPwZ3ubhetgW5NxMxRPypOeLh-wtYttT07xDaHuLvS_V5AKdrSF7MVWavtk816Y-UF3WlP54jdY0ov975cQaE8AZQSrK7_2eqL2RwvzBa2xinyZcc9RPTR7wv0hVR39nQA5TmVhl6eD233AemnLORPPawdaxtZEtfG4NnQPt8Q5EkiaezdMkqCSiTHZ4NUSHRwJY5JPCIlQ0p9-Dv9U3Z4xOeGH4eGXSOQYbcOCi3RC44abSYr7z5OG6YbhhnhGitAgn0wVg1ApzrXp21PPKKTYLjU-NLwcUuMN24ta0aMA9_SnNdd277I8OdaiZzmu1Kll_2dyp_9_WFew', props.refreshToken)
       .then(() => setLoading(false))
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [prefixPath])
@@ -30,13 +29,11 @@ const Browser = (props) => {
     props.getFile(path, props.userToken, props.refreshToken)
   }
 
-  const handleUploadFileDropzone = (files) => {
+  const handleUploadFile = (files) => {
     console.log(files[0].file)
     props.uploadFile(files[0].file, prefixPath, props.userToken, props.refreshToken)
     setModalShow(false)
   }
-
-  const getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
 
   return (
     <React.Fragment>
@@ -66,7 +63,7 @@ const Browser = (props) => {
                 styles={{ dropzone: { overflow: 'hidden' } }}
                 multiple={false}
                 maxFiles={1}
-                onSubmit={handleUploadFileDropzone}
+                onSubmit={handleUploadFile}
                 maxSizeBytes={5 * 1024 * 1024} />
             </ModalBody>
           </Modal>
